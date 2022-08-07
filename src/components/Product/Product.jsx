1a467b3d88602388132d0992c7db87af796ff75c
Ashik-Rahman100e <ashikrahman5515u@gmail.com>
2022-08-07 23:09:55 +0600

    UI Design Done

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,26 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import './Product.css';
 
-const Product = () => {
+const Product = ({product,handleAddToCart}) => {
+    // const {product,handleAddToCart} = props;
+    const{name,img,price,seller,ratings} = product;
+    
     return (
-        <div>
-            <h2>This is Product</h2>
+        <div className='product' >
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name' >{name}</p>
+                <p style={{fontSize:'25px',marginTop:'5px', marginBottom:'15px'}} >Price: ${price}</p>
+                <p><small>Manufacturer: {seller}</small></p>
+                <p><small>{ratings} stars</small></p>
+            </div>
+            <button className='btn-cart' 
+            onClick={() => handleAddToCart(product)} >
+                <p className='btn-text' >Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
